147522226939be30719b7f5b38da8a630df8f5e9
Aakaash SS <ssaakaash@icloud.com>
2021-07-27 15:55:41 +0530

    Added IDLE compatibility and option to quit

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,9 +4,15 @@
 
 import random
 from os import system, name
+import sys
+
+play = True                   # Sets the play state of the game
 
 # Clears the terminal screen
 def clear():
+    if 'idlelib' in sys.modules:
+        # Hacky way to clear screen in IDLE by printing 25 new lines.
+        print("\n" * 25)       
     if name == 'nt':            # For Windows
         _ = system('cls')
     else:                       # For MacOS, Linux
@@ -15,31 +21,35 @@ def clear():
 # Asks you to enter a number, catching the error if you type a non-number
 def askNumber(i: int):
     while True:
+        num = input(f"\nEnter your guess {i}: ").strip().lower()
         try:
-            return int(input(f"\nEnter your guess {i}: ").strip())
+            return int(num)
             break
         except ValueError:
-            print("Sorry, please enter a valid number.")
+            if num == 'quit':
+                break
+            else:
+                print("Sorry, please enter a valid number.")
 
 # Asks you whether you want to play again
 def playAgain():
     val = input("Do you want to play again? (y/n): ").strip().lower()
-    if val == 'y':
+    if val == 'y' or val == 'yes':
         clear()                 # Clears screen if yes
         return True
     else:
         return False
 
 clear()
+
 name = input("\nPlease enter your name: ").strip().lower()
 print(f"Hello {name.title()}, Let's play a game!\n")
 
 print("I'm gonna think of a number from 1 to 20, you have 6 tries to guess it.")
+print("If you want to quit, enter 'quit'.")
 print("Ready? Let's begin!")
 
-play_again = True                   # Sets the play state of the game
-
-while play_again:
+while play:
 
     number = random.randint(1, 21)  # Generates a random integer from 1-20
 
@@ -47,18 +57,24 @@ while play_again:
 
         guess = askNumber(i)
 
+        if not guess:
+            play = False
+            break
+
         if guess < number:
             print("The number is higher than your guess")
         elif guess > number:
             print("The number is lower than your guess")
         else:
             print("\nCongrats! You guessed the number!")
-            play_again = playAgain()
+            play = playAgain()
             break
+
+    if not guess: break
 
     if guess != number:
         print(f"\nSorry, the number was {number}")
-        play_again = playAgain()
+        play = playAgain()
         
 
 
